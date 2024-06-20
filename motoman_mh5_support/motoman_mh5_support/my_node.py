5cd16f6127959ba0631e69ba62651a7f2cba7774
tc-huang <tzu.chun.huang.tw@gmail.com>
2024-06-20 15:58:33 +0800

    fix: Change motoman_mh5_support from cpp  to python package

---
+++ motoman_mh5_support/motoman_mh5_support/my_node.py
@@ -0,0 +1,6 @@
+def main():
+    print('Hi from motoman_mh5_support.')
+
+
+if __name__ == '__main__':
+    main()
---
+++ motoman_mh5_support/setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+package_name = 'motoman_mh5_support'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='tc-huang',
+    maintainer_email='tzu.chun.huang.tw@gmail.com',
+    description='TODO: Package description',
+    license='Apache-2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'my_node = motoman_mh5_support.my_node:main'
+        ],
+    },
+)
