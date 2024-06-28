e465269cb2b37a63d2ffc740b5e76609ca0e381c
tc-huang <tzu.chun.huang.tw@gmail.com>
2024-06-29 00:55:11 +0800

    feat: Add common colors and materials for Motoman MH5 robot
    This commit adds common colors and materials for the Motoman MH5 robot. It includes the definition of colors such as yaskawa blue, white, yellow, and silver, along with corresponding material definitions. These additions are necessary for accurately representing the robot's appearance in simulation and visualization.

--- motoman_resources/setup.py
+++ motoman_resources/setup.py
@@ -1,4 +1,6 @@
 from setuptools import find_packages, setup
+from glob import glob
+import os
 
 package_name = 'motoman_resources'
 
@@ -10,6 +12,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'urdf'), glob('urdf/*')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
