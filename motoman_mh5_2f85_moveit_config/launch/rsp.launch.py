19a6f71147d55fa5e249a2af8f41632ed0686209
tc-huang <tzu.chun.huang.tw@gmail.com>
2024-06-29 00:56:14 +0800

    feat: Update configuration files for Motoman MH5 robot

---
+++ motoman_mh5_2f85_moveit_config/launch/rsp.launch.py
@@ -0,0 +1,7 @@
+from moveit_configs_utils import MoveItConfigsBuilder
+from moveit_configs_utils.launches import generate_rsp_launch
+
+
+def generate_launch_description():
+    moveit_config = MoveItConfigsBuilder("motoman_mh5_2f85", package_name="motoman_mh5_2f85_moveit_config").to_moveit_configs()
+    return generate_rsp_launch(moveit_config)
