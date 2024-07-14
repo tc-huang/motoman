9375ceca022f6fc388f2284d1662eddba04dd9f5
rl <p76124053@gs.ncku.edu.tw>
2024-07-14 17:28:47 +0800

    feat: Update more accurate calibration params

--- motoman_mh5_moveit_config/launch/calibration_camera_pose.launch.py
+++ motoman_mh5_moveit_config/launch/calibration_camera_pose.launch.py
@@ -1,5 +1,5 @@
 """ Static transform publisher acquired via MoveIt 2 hand-eye calibration """
-""" EYE-IN-HAND: link_t -> camera_color_optical_frame """
+""" EYE-IN-HAND: robotiq_85_base_link -> camera_color_optical_frame """
 from launch import LaunchDescription
 from launch_ros.actions import Node
 
@@ -12,29 +12,29 @@ def generate_launch_description() -> LaunchDescription:
             output="log",
             arguments=[
                 "--frame-id",
-                "link_t",
+                "robotiq_85_base_link",
                 "--child-frame-id",
                 "camera_color_optical_frame",
                 "--x",
-                "2.06982e-05",
+                "0.0205807",
                 "--y",
-                "-0.0505135",
+                "-0.0437127",
                 "--z",
-                "0.186187",
+                "-0.0400177",
                 "--qx",
-                "4.70777e-18",
+                "0.0167113",
                 "--qy",
-                "3.44107e-18",
+                "0.0207555",
                 "--qz",
-                "6.16893e-18",
+                "0.99632",
                 "--qw",
-                "1",
+                "0.0814677",
                 # "--roll",
-                # "9.41553e-18",
+                # "3.10292",
                 # "--pitch",
-                # "6.88213e-18",
+                # "3.1049",
                 # "--yaw",
-                # "1.23379e-17",
+                # "-0.162465",
             ],
         ),
     ]
